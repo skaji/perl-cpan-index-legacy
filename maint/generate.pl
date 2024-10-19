eff10ae2171dec30f27156066630785fa1aa2818
Shoichi Kaji <skaji@cpan.org>
2024-10-19 13:34:58 +0900

    perl-cpan-index-generate

--- maint/generate.pl
+++ maint/generate.pl
@@ -35,44 +35,10 @@ my %spec = (
     },
 );
 
-sub fixed_resolver ($spec) {
-    join ",", "Fixed", map { sprintf '%s@%s', $_, $spec->{$_} } sort keys $spec->%*;
-}
-
 sub run (@cmd) {
     warn "---> @cmd\n";
     0 == system @cmd or die;
 }
-
-sub generate_index ($spec, $dir) {
-    my @index;
-    my $visit = sub ($path, $state) {
-        return if $path !~ /install\.json$/;
-        state $JSON = JSON::XS->new;
-        my $install = $JSON->decode($path->slurp);
-        my $need;
-        for my $package (keys $install->{provides}->%*) {
-            $spec->{$package} and $need++;
-        }
-        return if !$need;
-        for my ($package, $option) ($install->{provides}->%*) {
-            push @index, {
-                package => $package,
-                version => $option->{version},
-                pathname => $install->{pathname},
-            };
-        }
-    };
-    Path::Tiny->new($dir)->visit($visit, { recurse => 1 });
-
-    my $out = "";
-    for my $index (sort { lc($a->{package}) cmp lc($b->{package}) } @index) {
-        $out .= sprintf "%s %s %s\n",
-            $index->{package}, $index->{version} || 'undef', $index->{pathname};
-    }
-    $out;
-}
-
 
 for my $version ("5.8.1", "5.10.1") {
     my $spec = $spec{$version};
@@ -80,18 +46,16 @@ for my $version ("5.8.1", "5.10.1") {
 
     File::Path::remove_tree $local_lib;
 
+    my $fixed = join ",", map { sprintf '%s@%s', $_, $spec->{$_} } sort keys $spec->%*;
     run "cpm",
         "install",
         "-L", $local_lib,
         "--target-perl", $version,
-        "--resolver", fixed_resolver($spec),
+        "--resolver", "Fixed,$fixed",
         (sort keys $spec->%*);
 
-    my $file = "../$version.txt";
-    warn "---> Writing $file\n";
-    open my $fh, ">", $file or die;
-
-    print {$fh} "# This is a CPAN index file for perl v$version\n";
-    print {$fh} "\n";
-    print {$fh} generate_index $spec, "$local_lib/lib/perl5";
+    run "perl-cpan-index-generate",
+        "--output", "../$version.txt",
+        "--include", (join ",", sort keys $spec->%*),
+        "$local_lib/lib/perl5";
 }
